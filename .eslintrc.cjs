208e62f4d5d11613009a445e2ee8d1f10dcd2464
liheng <1061555401@qq.com>
2022-09-08 23:18:00 +0800

    feat: project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  // root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="flex-1">
+    <input type="text" />
+  </aside>
+  <main class="flex-1">12323</main>
+</template>
+
+<style scoped></style>
